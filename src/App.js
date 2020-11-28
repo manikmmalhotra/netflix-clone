67db014b31ca5d98bf9251a22846ca8647684227
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-11-28 14:07:38 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Banner from './Banner';
+import requests from './request';
+import Row from './Row';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+
+      <Banner />
+      <Row isLarge title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanticMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      
     </div>
   );
 }
@@ -11,4 +24,4 @@ function App() {
 export default App;
 
 
-//2b0ef349de2bc856a20cac36b2083648
+//
